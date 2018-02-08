8543b2e9dd217a20ba7ee74c3bf097684326fc15
MrsFey <fey.gloria@marist.net>
2018-02-08 09:56:38 -0600

    adding print statement

--- TestingFeb8/ViewController.swift
+++ TestingFeb8/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("testt 2-8 " ) ;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
